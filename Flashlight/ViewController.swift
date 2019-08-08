21246dd394532402229822122efd680676bf243e
William Chen <chen.will7@gmail.com>
2019-08-08 14:00:17 -0700

    Add Flashlight Project

---
+++ Flashlight/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by William Chen on 7/19/19.
+//  Copyright © 2019 William Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var
+    HelloWorld: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        HelloWorld.setTitle("Flashlight", for: .normal)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
